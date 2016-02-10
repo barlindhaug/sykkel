c0d8538de49eee137627943371f86f5d72118c63
Fredrik Vraalsen <fredrik@vraalsen.no>
2016-02-10 13:05:29 +0100

    Changed challenge to 'February 2016'

--- src/sykkel/server.clj
+++ src/sykkel/server.clj
@@ -17,8 +17,8 @@
                (str
                  (let [data (core/go)]
                    (str
-                    "<h1>Iterate <strong>vinter</strong> sykkel challenge 2015</h1>"
-                    "<h2>\"Desember 2015\"</h2>"
+                    "<h1>Iterate <strong>vinter</strong> sykkel challenge 2015/16</h1>"
+                    "<h2>\"Februar 2016\"</h2>"
                     "<h3> Totalt: <strong>"(core/get-total data) "km</strong></h3>"
                     "<ol style=\"list-style-type: decimal;\">"
                     (reduce
