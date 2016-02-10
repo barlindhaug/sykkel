42fbe8615345433897f91f6449e47f2710f01343
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2016-02-10 15:46:02 +0100

    Update dependencies (including clojure 1.7)

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject sykkel "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
-                 [org.clojure/java.jdbc "0.3.6"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.4.2"]
                  [postgresql "9.1-901.jdbc4"]
                  [clj-http "0.9.2"]
-                 [clj-time "0.9.0"]
-                 [compojure "1.3.3"]
-                 [ring/ring-defaults "0.1.4"]
-                 [ring/ring-jetty-adapter "1.3.2"]]
+                 [clj-time "0.11.0"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler sykkel.server/app
          :auto-refresh? true}
